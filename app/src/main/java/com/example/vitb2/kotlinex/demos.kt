fb6d245532a740154cd321ebae66e2ca0d91a23c
Harshithaareddy <141898184+Harshithaareddy@users.noreply.github.com>
2024-06-10 08:44:50 +0530

    played with kotlin

---
+++ app/src/main/java/com/example/vitb2/kotlinex/demos.kt
@@ -0,0 +1,12 @@
+package com.example.vitb2.kotlinex
+
+fun main() {
+    val age = 18
+    val required = 18
+    if(age >= required)
+    {
+        println("can vote")
+    }else{
+        println("can't vote")
+    }
+}
